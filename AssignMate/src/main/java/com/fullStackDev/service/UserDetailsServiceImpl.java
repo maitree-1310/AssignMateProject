70a1ce127143473d6347bc261e60c056f018c907
Maitree b <mytree1013@gmail.com>
2024-03-07 17:02:10 +0530

    backend changes and frontend recreated because of failure in version compatibility

--- AssignMate/src/main/java/com/fullStackDev/service/UserDetailsServiceImpl.java
+++ AssignMate/src/main/java/com/fullStackDev/service/UserDetailsServiceImpl.java
@@ -4,10 +4,16 @@ import com.fullStackDev.domain.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserDetailsServiceImpl implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return new User();
+        User user = new User();
+        user.setUsername(username);
+        user.setPassword("password");
+        user.setId(1l);
+        return user;
     }
 }
