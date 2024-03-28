ddadfe905d25796f3920211c9a147236b295673d
Maitree b <mytree1013@gmail.com>
2024-03-28 16:04:08 +0530

    documentartion added to file

--- AssignMate/src/main/java/com/fullStackDev/assignMate/ServletInitializer.java
+++ AssignMate/src/main/java/com/fullStackDev/assignMate/ServletInitializer.java
@@ -1,13 +1,35 @@
 package com.fullStackDev.assignMate;
 
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
-public class ServletInitializer extends SpringBootServletInitializer {
+import static org.springframework.security.config.Customizer.withDefaults;
 
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(AssignMateApplication.class);
-	}
+@Configuration
+@EnableWebSecurity
+public class ServletInitializer  {
+
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authz) -> authz
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(withDefaults());
+        return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManagerBean(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager(); // Use AuthenticationManager from configuration
+    }
+
+
 
 }
